8b7c15ee4e13928d5fe3b4dc19ae0733020f6a3c
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2018-02-16 11:50:52 -0500

    Fixed most reported bugs

--- app/src/main/java/me/tagavari/airmessage/NotificationUtils.java
+++ app/src/main/java/me/tagavari/airmessage/NotificationUtils.java
@@ -116,7 +116,7 @@ class NotificationUtils {
 		//Checking if the Android version is below Oreo
 		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
 			//Setting the sound
-			if(playSound && PreferenceManager.getDefaultSharedPreferences(context).getBoolean(context.getResources().getString(R.string.preference_messagenotifications_sound_key), false)) notificationBuilder.setSound(Uri.parse(PreferenceManager.getDefaultSharedPreferences(context).getString(context.getResources().getString(R.string.preference_messagenotifications_sound_key), null)));
+			if(playSound) notificationBuilder.setSound(Uri.parse(PreferenceManager.getDefaultSharedPreferences(context).getString(context.getResources().getString(R.string.preference_messagenotifications_sound_key), null)));
 			
 			//Enabling vibration if it is enabled in the preferences
 			if(PreferenceManager.getDefaultSharedPreferences(context).getBoolean(context.getResources().getString(R.string.preference_messagenotifications_vibrate_key), false)) notificationBuilder.setVibrate(new long[]{1000, 1000});
