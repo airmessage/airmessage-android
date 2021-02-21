3e003a492659712ef25fef6d5944d996de9bb475
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2021-02-21 08:33:44 -0500

    Launch SystemMessageImportService as foreground service

--- app/src/main/java/me/tagavari/airmessage/receiver/DefaultMessagingAppChangedReceiver.java
+++ app/src/main/java/me/tagavari/airmessage/receiver/DefaultMessagingAppChangedReceiver.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.provider.Telephony;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 
 import me.tagavari.airmessage.MainApplication;
 import me.tagavari.airmessage.activity.Preferences;
@@ -31,7 +32,7 @@ public class DefaultMessagingAppChangedReceiver extends BroadcastReceiver {
 				Preferences.setPreferenceTextMessageIntegration(context, true);
 				
 				//Starting the import service
-				context.startService(new Intent(context, SystemMessageImportService.class).setAction(SystemMessageImportService.selfIntentActionImport));
+				ContextCompat.startForegroundService(context, new Intent(context, SystemMessageImportService.class).setAction(SystemMessageImportService.selfIntentActionImport));
 			}
 		} else {
 			//Checking if text message integration is in an invalid state (enabled, but permissions are missing)
@@ -40,7 +41,7 @@ public class DefaultMessagingAppChangedReceiver extends BroadcastReceiver {
 				Preferences.setPreferenceTextMessageIntegration(context, false);
 				
 				//Clearing the database of text messages
-				context.startService(new Intent(context, SystemMessageImportService.class).setAction(SystemMessageImportService.selfIntentActionDelete));
+				ContextCompat.startForegroundService(context, new Intent(context, SystemMessageImportService.class).setAction(SystemMessageImportService.selfIntentActionDelete));
 			}
 		}
 	}
