b140d5f6e932be495e539f1b2440a9717d871c90
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-02-18 12:56:46 -0500

    Fix bad ConnectionFeature IntDef

--- app/src/main/java/me/tagavari/airmessage/enums/ConnectionFeature.java
+++ app/src/main/java/me/tagavari/airmessage/enums/ConnectionFeature.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.SOURCE)
-@IntDef({ConnectionFeature.idBasedRetrieval, ConnectionFeature.payloadPushNotifications})
+@IntDef({ConnectionFeature.idBasedRetrieval, ConnectionFeature.payloadPushNotifications, ConnectionFeature.remoteUpdates, ConnectionFeature.faceTime})
 public @interface ConnectionFeature {
 	int idBasedRetrieval = 0; //Retrieve messages using an ID range
 	int payloadPushNotifications = 1; //Receive FCM messages with message content
