530b3751fc718074e8333374f0bb98c60bdfd178
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-07-17 18:43:15 -0400

    Extend MultiLineSwitchPreference from non-appcompat preference

--- app/src/main/java/me/tagavari/airmessage/view/MultiLineSwitchPreference.java
+++ app/src/main/java/me/tagavari/airmessage/view/MultiLineSwitchPreference.java
@@ -3,10 +3,13 @@ package me.tagavari.airmessage.view;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.preference.PreferenceViewHolder;
+import androidx.preference.SwitchPreference;
 import androidx.preference.SwitchPreferenceCompat;
 
-public class MultiLineSwitchPreference extends SwitchPreferenceCompat {
+public class MultiLineSwitchPreference extends SwitchPreference {
 	public MultiLineSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
@@ -20,7 +23,7 @@ public class MultiLineSwitchPreference extends SwitchPreferenceCompat {
 	}
 	
 	@Override
-	public void onBindViewHolder(PreferenceViewHolder holder) {
+	public void onBindViewHolder(@NonNull PreferenceViewHolder holder) {
 		super.onBindViewHolder(holder);
 		TextView textView = (TextView) holder.findViewById(android.R.id.title);
 		if(textView != null) textView.setSingleLine(false);
