35af32654f6e5c7b351a154077a222ca3a2b0b72
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-08-02 07:24:10 -0400

    Remove unused file export service

--- app/src/main/java/me/tagavari/airmessage/helper/ExternalStorageHelper.kt
+++ app/src/main/java/me/tagavari/airmessage/helper/ExternalStorageHelper.kt
@@ -1,24 +1,12 @@
 package me.tagavari.airmessage.helper
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import me.tagavari.airmessage.service.FileExportService
 import me.tagavari.airmessage.service.UriExportService
 import java.io.File
 
 object ExternalStorageHelper {
-	/**
-	 * Exports a file to the app's public directory in a user-friendly way
-	 */
-	fun exportFile(context: Context, file: File) {
-		val intent = Intent(context, FileExportService::class.java).apply {
-			putExtra(FileExportService.intentParamPath, file.path)
-		}
-		context.startService(intent)
-	}
-	
 	/**
 	 * Exports a file to the specified URI in a user-friendly way
 	 */
