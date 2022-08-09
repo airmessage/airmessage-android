3218fc0c57d07bfb3f29beb61010df6286a5259f
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-08-09 11:40:59 -0400

    Use iOS-style messaging app bars

--- app/src/main/java/me/tagavari/airmessage/compose/component/MessagingScreen.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/component/MessagingScreen.kt
@@ -1,11 +1,14 @@
 package me.tagavari.airmessage.compose.component
 
 import android.app.Application
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
@@ -31,15 +34,6 @@ fun MessagingScreen(
 	var showContentPicker by remember { mutableStateOf(false) }
 	var collapseInputButtons by remember { mutableStateOf(false) }
 	
-	val context = LocalContext.current
-	
-	//Load the conversation from its ID
-	val conversation by produceState<ConversationInfo?>(initialValue = null, conversationID) {
-		value = withContext(Dispatchers.IO) {
-			DatabaseManager.getInstance().fetchConversationInfo(context, conversationID)
-		}
-	}
-	
 	val application = LocalContext.current.applicationContext as Application
 	val viewModel = viewModel<MessagingViewModel>(factory = MessagingViewModelFactory(application, conversationID))
 	
@@ -53,20 +47,34 @@ fun MessagingScreen(
 	Scaffold(
 		topBar = {
 			Surface(tonalElevation = 2.dp) {
-				SmallTopAppBar(
+				CenterAlignedTopAppBar(
+					modifier = Modifier.height(120.dp).statusBarsPadding(),
 					title = {
-						viewModel.conversationTitle?.let {
-							Text(it)
+						Column(
+							horizontalAlignment = Alignment.CenterHorizontally,
+							verticalArrangement = Arrangement.Center,
+						) {
+							viewModel.conversation?.let { conversation ->
+								UserIconGroup(members = conversation.members)
+							}
+							
+							Spacer(modifier = Modifier.height(2.dp))
+							
+							viewModel.conversationTitle?.let { title ->
+								Text(
+									text = title,
+									style = MaterialTheme.typography.bodySmall
+								)
+							}
 						}
 					},
-					modifier = Modifier.statusBarsPadding(),
 					navigationIcon = navigationIcon
 				)
 			}
 		},
 		content = { paddingValues ->
 			Column {
-				conversation?.let { conversation ->
+				viewModel.conversation?.let { conversation ->
 					MessageList(
 						modifier = Modifier
 							.weight(1F)
@@ -86,8 +94,8 @@ fun MessagingScreen(
 					onChangeShowContentPicker = { showContentPicker = it },
 					collapseButtons = collapseInputButtons,
 					onChangeCollapseButtons = { collapseInputButtons = it },
-					serviceHandler = conversation?.serviceHandler,
-					serviceType = conversation?.serviceType,
+					serviceHandler = viewModel.conversation?.serviceHandler,
+					serviceType = viewModel.conversation?.serviceType,
 					floating = !isScrolledToBottom
 				)
 			}
