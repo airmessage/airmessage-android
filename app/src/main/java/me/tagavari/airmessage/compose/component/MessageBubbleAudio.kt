d4b7e6067f6ad4300cf90a849869d475b011cc92
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-08-28 10:19:55 -0400

    Cache audio amplitudes

--- app/src/main/java/me/tagavari/airmessage/compose/component/MessageBubbleAudio.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/component/MessageBubbleAudio.kt
@@ -1,5 +1,6 @@
 package me.tagavari.airmessage.compose.component
 
+import android.util.LruCache
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
@@ -8,12 +9,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import me.tagavari.airmessage.R
 import me.tagavari.airmessage.compose.remember.AudioPlaybackState
 import me.tagavari.airmessage.helper.AudioDecodeHelper
 import me.tagavari.airmessage.util.MessagePartFlow
 import java.io.File
+
+private val audioAmplitudeCacheMutex = Mutex()
+private val audioAmplitudeCache = object : LruCache<File, List<Int>>(1024 * 1024) {
+	override fun sizeOf(key: File, value: List<Int>)
+			= value.size * Int.SIZE_BYTES
+}
 
 @Composable
 fun MessageBubbleAudio(
@@ -37,9 +46,22 @@ fun MessageBubbleAudio(
 			
 			//Get the amplitude list
 			val amplitudeList by produceState<List<Int>?>(null) {
-				@Suppress("BlockingMethodInNonBlockingContext")
-				value = withContext(Dispatchers.IO) {
-					AudioDecodeHelper.getAmplitudeList(file)
+				audioAmplitudeCacheMutex.withLock {
+					//Look up a previous value in the cache
+					audioAmplitudeCache[file]?.let {
+						value = it
+						return@withLock
+					}
+					
+					//Process the file
+					@Suppress("BlockingMethodInNonBlockingContext")
+					val amplitudeList = withContext(Dispatchers.IO) {
+						AudioDecodeHelper.getAmplitudeList(file)
+					}
+					
+					//Save the value in the cache
+					audioAmplitudeCache.put(file, amplitudeList)
+					value = amplitudeList
 				}
 			}
 			
