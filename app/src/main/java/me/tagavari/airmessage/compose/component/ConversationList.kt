b559a4040ceb7eec21974cc7e83ab65f7768ade7
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-08-29 13:14:35 -0400

    Update conversation list action bar design

--- app/src/main/java/me/tagavari/airmessage/compose/component/ConversationList.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/component/ConversationList.kt
@@ -1,6 +1,7 @@
 package me.tagavari.airmessage.compose.component
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,8 +19,10 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import me.tagavari.airmessage.R
+import me.tagavari.airmessage.compose.ui.theme.AirMessageAndroidTheme
 import me.tagavari.airmessage.enums.ConversationState
 import me.tagavari.airmessage.enums.ServiceHandler
 import me.tagavari.airmessage.enums.ServiceType
@@ -53,117 +56,127 @@ fun ConversationList(
 	Scaffold(
 		modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
 		topBar = {
-			if(!isActionMode) {
-				LargeTopAppBar(
-					title = { Text(stringResource(id = R.string.app_name)) },
-					scrollBehavior = scrollBehavior,
-					actions = {
-						var menuOpen by remember { mutableStateOf(false) }
-						
-						IconButton(onClick = { menuOpen = !menuOpen }) {
-							Icon(Icons.Default.MoreVert, contentDescription = "")
-						}
-						
-						DropdownMenu(
-							expanded = menuOpen,
-							onDismissRequest = { menuOpen = false }
-						) {
-							DropdownMenuItem(
-								text = { Text(stringResource(id = R.string.screen_settings)) },
-								onClick = {
-									menuOpen = false
-									onNavigateSettings()
-								}
-							)
-						}
-					}
-				)
-			} else {
-				//Associate a conversation's ID to itself
-				val conversationsMap = remember(conversations) {
-					conversations?.getOrNull()?.associateBy { it.localID } ?: mapOf()
-				}
-				
-				LargeTopAppBar(
-					title = {
-						Text(selectedConversations.size.let { size ->
-							pluralStringResource(id = R.plurals.message_selectioncount, size, size)
-						})
-					},
-					scrollBehavior = scrollBehavior,
-					navigationIcon = {
-						IconButton(onClick = { stopActionMode() }) {
-							Icon(
-								imageVector = Icons.Filled.Close,
-								contentDescription = stringResource(id = android.R.string.cancel)
-							)
-						}
-					},
-					actions = {
-						data class SelectionContents(
-							val hasMuted: Boolean,
-							val hasUnmuted: Boolean,
-							val hasArchived: Boolean,
-							val hasUnarchived: Boolean
-						)
-						
-						val selectionContents = remember(selectedConversations) {
-							val conversationsSequence = selectedConversations
-								.asSequence()
-								.mapNotNull { conversationsMap[it] }
+			Crossfade(targetState = isActionMode) { isActionMode ->
+				if(!isActionMode) {
+					LargeTopAppBar(
+						title = {
+							Text(stringResource(id = R.string.app_name), fontWeight = FontWeight.Medium)
+						},
+						scrollBehavior = scrollBehavior,
+						actions = {
+							var menuOpen by remember { mutableStateOf(false) }
 							
-							SelectionContents(
-								hasMuted = conversationsSequence.any { it.isMuted },
-								hasUnmuted = conversationsSequence.any { !it.isMuted },
-								hasArchived = conversationsSequence.any { it.isArchived },
-								hasUnarchived = conversationsSequence.any { !it.isArchived }
-							)
-						}
-						
-						if(selectionContents.hasMuted) {
-							IconButton(onClick = {}) {
-								Icon(
-									imageVector = Icons.Outlined.NotificationsActive,
-									contentDescription = stringResource(id = R.string.action_unmute)
+							IconButton(onClick = { menuOpen = !menuOpen }) {
+								Icon(Icons.Default.MoreVert, contentDescription = "")
+							}
+							
+							DropdownMenu(
+								expanded = menuOpen,
+								onDismissRequest = { menuOpen = false }
+							) {
+								DropdownMenuItem(
+									text = { Text(stringResource(id = R.string.screen_settings)) },
+									onClick = {
+										menuOpen = false
+										onNavigateSettings()
+									}
 								)
 							}
-						}
-						
-						if(selectionContents.hasUnmuted) {
-							IconButton(onClick = {}) {
-								Icon(
-									imageVector = Icons.Outlined.NotificationsOff,
-									contentDescription = stringResource(id = R.string.action_mute)
-								)
-							}
-						}
-						
-						if(selectionContents.hasArchived) {
-							IconButton(onClick = {}) {
-								Icon(
-									imageVector = Icons.Outlined.Unarchive,
-									contentDescription = stringResource(id = R.string.action_unarchive)
-								)
-							}
-						}
-						
-						if(selectionContents.hasUnarchived) {
-							IconButton(onClick = {}) {
-								Icon(
-									imageVector = Icons.Outlined.Archive,
-									contentDescription = stringResource(id = R.string.action_archive)
-								)
-							}
-						}
-						
-						IconButton(onClick = {}) {
-							Icon(
-								imageVector = Icons.Outlined.Delete,
-								contentDescription = stringResource(id = R.string.action_delete)
-							)
-						}
+						},
+						colors = TopAppBarDefaults.largeTopAppBarColors(
+							titleContentColor = MaterialTheme.colorScheme.primary
+						)
+					)
+				} else {
+					//Associate a conversation's ID to itself
+					val conversationsMap = remember(conversations) {
+						conversations?.getOrNull()?.associateBy { it.localID } ?: mapOf()
 					}
-				)
+					
+					LargeTopAppBar(
+						title = {
+							Text(selectedConversations.size.let { size ->
+								pluralStringResource(id = R.plurals.message_selectioncount, size, size)
+							})
+						},
+						scrollBehavior = scrollBehavior,
+						navigationIcon = {
+							IconButton(onClick = { stopActionMode() }) {
+								Icon(
+									imageVector = Icons.Filled.Close,
+									contentDescription = stringResource(id = android.R.string.cancel)
+								)
+							}
+						},
+						actions = {
+							data class SelectionContents(
+								val hasMuted: Boolean,
+								val hasUnmuted: Boolean,
+								val hasArchived: Boolean,
+								val hasUnarchived: Boolean
+							)
+							
+							val selectionContents = remember(selectedConversations) {
+								val conversationsSequence = selectedConversations
+									.asSequence()
+									.mapNotNull { conversationsMap[it] }
+								
+								SelectionContents(
+									hasMuted = conversationsSequence.any { it.isMuted },
+									hasUnmuted = conversationsSequence.any { !it.isMuted },
+									hasArchived = conversationsSequence.any { it.isArchived },
+									hasUnarchived = conversationsSequence.any { !it.isArchived }
+								)
+							}
+							
+							if(selectionContents.hasMuted) {
+								IconButton(onClick = {}) {
+									Icon(
+										imageVector = Icons.Outlined.NotificationsActive,
+										contentDescription = stringResource(id = R.string.action_unmute)
+									)
+								}
+							}
+							
+							if(selectionContents.hasUnmuted) {
+								IconButton(onClick = {}) {
+									Icon(
+										imageVector = Icons.Outlined.NotificationsOff,
+										contentDescription = stringResource(id = R.string.action_mute)
+									)
+								}
+							}
+							
+							if(selectionContents.hasArchived) {
+								IconButton(onClick = {}) {
+									Icon(
+										imageVector = Icons.Outlined.Unarchive,
+										contentDescription = stringResource(id = R.string.action_unarchive)
+									)
+								}
+							}
+							
+							if(selectionContents.hasUnarchived) {
+								IconButton(onClick = {}) {
+									Icon(
+										imageVector = Icons.Outlined.Archive,
+										contentDescription = stringResource(id = R.string.action_archive)
+									)
+								}
+							}
+							
+							IconButton(onClick = {}) {
+								Icon(
+									imageVector = Icons.Outlined.Delete,
+									contentDescription = stringResource(id = R.string.action_delete)
+								)
+							}
+						},
+						colors = TopAppBarDefaults.largeTopAppBarColors(
+							scrolledContainerColor = MaterialTheme.colorScheme.primaryContainer
+						)
+					)
+				}
 			}
 		},
 		content = { innerPadding ->
@@ -251,49 +264,55 @@ fun ConversationList(
 @Preview(name = "Conversation list")
 @Composable
 fun PreviewConversationList() {
-	ConversationList(
-		conversations = Result.success(listOf(
-			ConversationInfo(
-				localID = 0,
-				guid = null,
-				externalID = -1,
-				state = ConversationState.ready,
-				serviceHandler = ServiceHandler.appleBridge,
-				serviceType = ServiceType.appleMessage,
-				conversationColor = 0xFFFF1744.toInt(),
-				members = mutableListOf(
-					MemberInfo("test", 0xFFFF1744.toInt())
-				),
-				title = "A cool conversation",
-				unreadMessageCount = 1,
-				isArchived = false,
-				isMuted = true,
-				messagePreview = ConversationPreview.Message(
-					date = System.currentTimeMillis(),
-					isOutgoing = false,
-					message = "Test message",
-					subject = null,
-					attachments = listOf(),
-					sendStyle = null,
-					isError = false
+	AirMessageAndroidTheme {
+		ConversationList(
+			conversations = Result.success(listOf(
+				ConversationInfo(
+					localID = 0,
+					guid = null,
+					externalID = -1,
+					state = ConversationState.ready,
+					serviceHandler = ServiceHandler.appleBridge,
+					serviceType = ServiceType.appleMessage,
+					conversationColor = 0xFFFF1744.toInt(),
+					members = mutableListOf(
+						MemberInfo("test", 0xFFFF1744.toInt())
+					),
+					title = "A cool conversation",
+					unreadMessageCount = 1,
+					isArchived = false,
+					isMuted = true,
+					messagePreview = ConversationPreview.Message(
+						date = System.currentTimeMillis(),
+						isOutgoing = false,
+						message = "Test message",
+						subject = null,
+						attachments = listOf(),
+						sendStyle = null,
+						isError = false
+					)
 				)
-			)
-		))
-	)
+			))
+		)
+	}
 }
 
 @Preview(name = "Loading state")
 @Composable
 fun PreviewConversationListLoading() {
-	ConversationList(
-		conversations = null
-	)
+	AirMessageAndroidTheme {
+		ConversationList(
+			conversations = null
+		)
+	}
 }
 
 @Preview(name = "Error state")
 @Composable
 fun PreviewConversationListError() {
-	ConversationList(
-		conversations = Result.failure(Exception("Failed to load conversations"))
-	)
+	AirMessageAndroidTheme {
+		ConversationList(
+			conversations = Result.failure(Exception("Failed to load conversations"))
+		)
+	}
 }
