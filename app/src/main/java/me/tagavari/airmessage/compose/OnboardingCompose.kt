6937b898ebd5ba542bf9ff6540627ee1e73ad056
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-09-09 11:13:59 -0400

    Use AirMessageAndroidTheme

--- app/src/main/java/me/tagavari/airmessage/compose/OnboardingCompose.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/OnboardingCompose.kt
@@ -15,6 +15,7 @@ import me.tagavari.airmessage.compose.component.onboarding.OnboardingConnect
 import me.tagavari.airmessage.compose.component.onboarding.OnboardingManual
 import me.tagavari.airmessage.compose.component.onboarding.OnboardingManualState
 import me.tagavari.airmessage.compose.component.onboarding.OnboardingWelcome
+import me.tagavari.airmessage.compose.ui.theme.AirMessageAndroidTheme
 import me.tagavari.airmessage.connection.comm5.ProxyConnect
 import soup.compose.material.motion.MaterialSharedAxisX
 
@@ -24,44 +25,46 @@ class OnboardingCompose : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		
 		setContent {
-			var screen by rememberSaveable { mutableStateOf(OnboardingComposeScreen.WELCOME) }
-			
-			//Navigate backwards when back is pressed
-			BackHandler(enabled = screen != OnboardingComposeScreen.WELCOME) {
-				screen = OnboardingComposeScreen.WELCOME
-			}
-			
-			MaterialSharedAxisX(
-				targetState = screen,
-				forward = screen != OnboardingComposeScreen.WELCOME,
-			) { animatedScreen ->
-				when(animatedScreen) {
-					OnboardingComposeScreen.WELCOME -> {
-						OnboardingWelcome(
-							modifier = Modifier.fillMaxSize(),
-							showGoogle = ProxyConnect.isAvailable,
-							onClickGoogle = { screen = OnboardingComposeScreen.CONNECT },
-							onClickManual = { screen = OnboardingComposeScreen.MANUAL }
-						)
-					}
-					OnboardingComposeScreen.CONNECT -> {
-						OnboardingConnect(
-							modifier = Modifier.fillMaxSize(),
-							onEnterPassword = {},
-							onReconnect = {}
-						)
-					}
-					OnboardingComposeScreen.MANUAL -> {
-						OnboardingManual(
-							modifier = Modifier.fillMaxSize(),
-							state = OnboardingManualState.Idle,
-							onConnect = {},
-							onReset = {},
-							onCancel = {
-								screen = OnboardingComposeScreen.WELCOME
-							},
-							onFinish = {}
-						)
+			AirMessageAndroidTheme {
+				var screen by rememberSaveable { mutableStateOf(OnboardingComposeScreen.WELCOME) }
+				
+				//Navigate backwards when back is pressed
+				BackHandler(enabled = screen != OnboardingComposeScreen.WELCOME) {
+					screen = OnboardingComposeScreen.WELCOME
+				}
+				
+				MaterialSharedAxisX(
+					targetState = screen,
+					forward = screen != OnboardingComposeScreen.WELCOME,
+				) { animatedScreen ->
+					when(animatedScreen) {
+						OnboardingComposeScreen.WELCOME -> {
+							OnboardingWelcome(
+								modifier = Modifier.fillMaxSize(),
+								showGoogle = ProxyConnect.isAvailable,
+								onClickGoogle = { screen = OnboardingComposeScreen.CONNECT },
+								onClickManual = { screen = OnboardingComposeScreen.MANUAL }
+							)
+						}
+						OnboardingComposeScreen.CONNECT -> {
+							OnboardingConnect(
+								modifier = Modifier.fillMaxSize(),
+								onEnterPassword = {},
+								onReconnect = {}
+							)
+						}
+						OnboardingComposeScreen.MANUAL -> {
+							OnboardingManual(
+								modifier = Modifier.fillMaxSize(),
+								state = OnboardingManualState.Idle,
+								onConnect = {},
+								onReset = {},
+								onCancel = {
+									screen = OnboardingComposeScreen.WELCOME
+								},
+								onFinish = {}
+							)
+						}
 					}
 				}
 			}
