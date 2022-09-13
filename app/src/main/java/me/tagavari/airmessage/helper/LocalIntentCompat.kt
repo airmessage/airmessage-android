73ec9655f8897b873eec41aafa7cad0f72de974f
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-09-12 23:36:10 -0400

    Update LocalIntentCompat.kt

--- app/src/main/java/me/tagavari/airmessage/helper/LocalIntentCompat.kt
+++ app/src/main/java/me/tagavari/airmessage/helper/LocalIntentCompat.kt
@@ -5,11 +5,11 @@ import android.os.Build
 import android.os.Bundle
 
 inline fun <reified T> Intent.getParcelableExtraCompat(key: String): T? = when {
-	Build.VERSION.SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
+	Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, T::class.java)
 	else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
 }
 
 inline fun <reified T> Bundle.getParcelableCompat(key: String): T? = when {
-	Build.VERSION.SDK_INT >= 33 -> getParcelable(key, T::class.java)
+	Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelable(key, T::class.java)
 	else -> @Suppress("DEPRECATION") getParcelable(key) as? T
 }
