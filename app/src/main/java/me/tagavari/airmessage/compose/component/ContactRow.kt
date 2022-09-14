6a048d59c4702b82ea5f61dd91a98c5ac06fe6e7
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-09-14 19:58:14 -0400

    Use custom alert dialog for address selection

--- app/src/main/java/me/tagavari/airmessage/compose/component/ContactRow.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/component/ContactRow.kt
@@ -3,20 +3,23 @@ package me.tagavari.airmessage.compose.component
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import me.tagavari.airmessage.R
 import me.tagavari.airmessage.compose.ui.theme.AirMessageAndroidTheme
 import me.tagavari.airmessage.util.AddressInfo
@@ -42,33 +45,30 @@ fun ContactRow(
 	}
 	
 	if(showAddressSelector) {
-		AlertDialog(
+		ContentAlertDialog(
 			onDismissRequest = { showAddressSelector = false },
-			confirmButton = {},
 			title = {
 				Text(stringResource(id = R.string.imperative_selectdestination))
-			},
-			text = {
-				Column {
-					for(address in contact.addresses) {
-						Row(
-							modifier = Modifier
-								.clip(RoundedCornerShape(16.dp))
-								.clickable {
-									onSelectAddress(address)
-									showAddressSelector = false
-								}
-								.fillMaxWidth()
-								.height(56.dp)
-								.padding(horizontal = 16.dp),
-							verticalAlignment = Alignment.CenterVertically
-						) {
-							Text(address.getDisplay(LocalContext.current.resources))
-						}
+			}
+		) {
+			Column {
+				for(address in contact.addresses) {
+					Row(
+						modifier = Modifier
+							.clickable {
+								onSelectAddress(address)
+								showAddressSelector = false
+							}
+							.fillMaxWidth()
+							.height(48.dp)
+							.padding(horizontal = 24.dp),
+						verticalAlignment = Alignment.CenterVertically
+					) {
+						Text(address.getDisplay(LocalContext.current.resources))
 					}
 				}
 			}
-		)
+		}
 	}
 	
 	Row(
