e6f931f1b75b3a5116b773f3fabe542393eefae9
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-09-17 19:42:10 -0400

    Display text emojis on their own

--- app/src/main/java/me/tagavari/airmessage/compose/component/MessageBubbleText.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/component/MessageBubbleText.kt
@@ -9,16 +9,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.material.textview.MaterialTextView
 import me.tagavari.airmessage.compose.ui.theme.AirMessageAndroidTheme
 import me.tagavari.airmessage.helper.LinkifyHelper
+import me.tagavari.airmessage.helper.StringHelper
 import me.tagavari.airmessage.util.CustomTabsLinkTransformationMethod
 import me.tagavari.airmessage.util.MessagePartFlow
 
@@ -46,72 +49,83 @@ fun MessageBubbleText(
 		onSetSelected(!flow.isSelected)
 	}
 	
-	Surface(
-		color = colors.background,
-		shape = flow.bubbleShape,
-		contentColor = colors.foreground
-	) {
-		Column(
-			modifier = Modifier
-				.heightIn(min = 40.dp)
-				.padding(horizontal = 12.dp, vertical = 8.dp),
-			verticalArrangement = Arrangement.Center
+	val isSingleEmoji = remember(subject, text) {
+		subject == null && text != null && StringHelper.stringContainsOnlyEmoji(text)
+	}
+	
+	if(isSingleEmoji) {
+		Text(
+			text = text!!,
+			fontSize = 48.sp
+		)
+	} else {
+		Surface(
+			color = colors.background,
+			shape = flow.bubbleShape,
+			contentColor = colors.foreground
 		) {
-			val typography = MaterialTheme.typography.bodyLarge
-			
-			//Subject text
-			if(subject != null) {
-				Text(
-					text = subject,
-					style = typography.copy(fontWeight = FontWeight.Bold)
-				)
+			Column(
+				modifier = Modifier
+					.heightIn(min = 40.dp)
+					.padding(horizontal = 12.dp, vertical = 8.dp),
+				verticalArrangement = Arrangement.Center
+			) {
+				val typography = MaterialTheme.typography.bodyLarge
 				
-				Spacer(modifier = Modifier.height(8.dp))
-			}
-			
-			//Body text
-			if(text != null) {
-				val trimmedText = text.trim()
-
-				val context = LocalContext.current
-				val linkifiedText by produceState(initialValue = SpannableString(trimmedText), trimmedText) {
-					value = LinkifyHelper.linkifyText(context, trimmedText)
+				//Subject text
+				if(subject != null) {
+					Text(
+						text = subject,
+						style = typography.copy(fontWeight = FontWeight.Bold)
+					)
+					
+					Spacer(modifier = Modifier.height(8.dp))
 				}
 				
-				//Use Android text view for Spannable support
-				val textFont = MaterialTheme.typography.bodyLarge.fontSize
-				assert(textFont.isSp) { "Text font is not in SP units!" }
-				AndroidView(
-					factory = { ctx ->
-						MaterialTextView(ctx).apply {
-							transformationMethod = CustomTabsLinkTransformationMethod()
-							movementMethod = LinkMovementMethod.getInstance()
-							textSize = textFont.value
-						}
-					},
-					update = { view ->
-						//Set view color
-						val color = colors.foreground.toArgb()
-						view.setTextColor(color)
-						view.setLinkTextColor(color)
-						
-						//Set linkified text
-						view.text = linkifiedText
-						
-						if(flow.isSelected) {
-							view.setOnClickListener {
-								onClick()
-							}
-						} else {
-							view.isClickable = false
-						}
-						
-						view.setOnLongClickListener {
-							onLongClick()
-							true
-						}
+				//Body text
+				if(text != null) {
+					val trimmedText = text.trim()
+					
+					val context = LocalContext.current
+					val linkifiedText by produceState(initialValue = SpannableString(trimmedText), trimmedText) {
+						value = LinkifyHelper.linkifyText(context, trimmedText)
 					}
-				)
+					
+					//Use Android text view for Spannable support
+					val textFont = MaterialTheme.typography.bodyLarge.fontSize
+					assert(textFont.isSp) { "Text font is not in SP units!" }
+					AndroidView(
+						factory = { ctx ->
+							MaterialTextView(ctx).apply {
+								transformationMethod = CustomTabsLinkTransformationMethod()
+								movementMethod = LinkMovementMethod.getInstance()
+								textSize = textFont.value
+							}
+						},
+						update = { view ->
+							//Set view color
+							val color = colors.foreground.toArgb()
+							view.setTextColor(color)
+							view.setLinkTextColor(color)
+							
+							//Set linkified text
+							view.text = linkifiedText
+							
+							if(flow.isSelected) {
+								view.setOnClickListener {
+									onClick()
+								}
+							} else {
+								view.isClickable = false
+							}
+							
+							view.setOnLongClickListener {
+								onLongClick()
+								true
+							}
+						}
+					)
+				}
 			}
 		}
 	}
@@ -149,6 +163,25 @@ private fun PreviewMessageBubbleSubject() {
 			),
 			subject = "An important message",
 			text = "Hello there friend!",
+			onSetSelected = {}
+		)
+	}
+}
+
+@Preview
+@Composable
+private fun PreviewMessageBubbleEmoji() {
+	AirMessageAndroidTheme {
+		MessageBubbleText(
+			flow = MessagePartFlow(
+				isOutgoing = true,
+				isSelected = false,
+				anchorBottom = false,
+				anchorTop = false,
+				tintRatio = 0F
+			),
+			subject = null,
+			text = "\ud83d\udc08",
 			onSetSelected = {}
 		)
 	}
