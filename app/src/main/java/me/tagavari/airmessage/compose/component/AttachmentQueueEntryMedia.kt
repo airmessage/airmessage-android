5804c63aebd797faf8693d184b5a679e92a2e5f7
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-10-19 09:17:43 -0400

    Show play icon for queued video files

--- app/src/main/java/me/tagavari/airmessage/compose/component/AttachmentQueueEntryMedia.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/component/AttachmentQueueEntryMedia.kt
@@ -2,14 +2,22 @@ package me.tagavari.airmessage.compose.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
 import coil.request.ImageRequest
+import me.tagavari.airmessage.R
+import me.tagavari.airmessage.helper.FileHelper
 import me.tagavari.airmessage.messaging.QueuedFile
 
 @Composable
@@ -28,6 +36,21 @@ fun AttachmentQueueEntryMedia(
 			.crossfade(true)
 			.build(),
 		contentDescription = null,
+		success = {
+			SubcomposeAsyncImageContent()
+			
+			//Show play icon for video files
+			if(FileHelper.compareMimeTypes(queuedFile.fileType, "video/*")) {
+				Icon(
+					modifier = Modifier
+						.size(48.dp)
+						.align(Alignment.Center),
+					painter = painterResource(id = R.drawable.play_circle_rounded),
+					tint = Color(0xFFFFFFFF),
+					contentDescription = null
+				)
+			}
+		},
 		error = { fallback() }
 	)
 }
