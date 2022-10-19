e1355a63f0f92e32cce48a2bd9ece7f0a91b8557
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-10-19 09:14:23 -0400

    Implement AttachmentQueueEntryFile

--- app/src/main/java/me/tagavari/airmessage/compose/component/AttachmentQueueEntry.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/component/AttachmentQueueEntry.kt
@@ -1,8 +1,8 @@
 package me.tagavari.airmessage.compose.component
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -12,13 +12,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import me.tagavari.airmessage.R
 import me.tagavari.airmessage.helper.FileHelper
 import me.tagavari.airmessage.messaging.QueuedFile
-import java.io.File
 
 @Composable
 fun AttachmentQueueEntry(
@@ -26,29 +24,42 @@ fun AttachmentQueueEntry(
 	onClick: () -> Unit,
 	onRemove: () -> Unit
 ) {
-	if(FileHelper.compareMimeTypes(queuedFile.fileType, "image/*")
-		|| FileHelper.compareMimeTypes(queuedFile.fileType, "video/*")) {
-		Box(
-			modifier = Modifier.fillMaxHeight()
-		) {
-			AttachmentQueueEntryMedia(queuedFile = queuedFile, onClick = onClick)
-			
-			if(queuedFile.localID != null) {
-				IconButton(
-					modifier = Modifier
-						.align(Alignment.TopEnd)
-						.offset(
-							x = (8 * if(LocalLayoutDirection.current === LayoutDirection.Ltr) 1 else -1).dp,
-							y = (-8).dp
-						),
-					onClick = onRemove
-				) {
-					Icon(
-						painter = painterResource(id = R.drawable.draft_close_circle_border),
-						contentDescription = stringResource(id = R.string.action_back),
-						tint = Color.Unspecified
-					)
-				}
+	val fallbackComponent = @Composable {
+		AttachmentQueueEntryFile(
+			queuedFile = queuedFile,
+			onClick = onClick
+		)
+	}
+	
+	Box(
+		modifier = Modifier.fillMaxHeight()
+	) {
+		if(FileHelper.compareMimeTypes(queuedFile.fileType, "image/*")
+			|| FileHelper.compareMimeTypes(queuedFile.fileType, "video/*")) {
+			AttachmentQueueEntryMedia(
+				queuedFile = queuedFile,
+				onClick = onClick,
+				fallback = fallbackComponent
+			)
+		} else {
+			fallbackComponent()
+		}
+		
+		if(queuedFile.localID != null) {
+			IconButton(
+				modifier = Modifier
+					.align(Alignment.TopEnd)
+					.offset(
+						x = (8 * if(LocalLayoutDirection.current === LayoutDirection.Ltr) 1 else -1).dp,
+						y = (-8).dp
+					),
+				onClick = onRemove
+			) {
+				Icon(
+					painter = painterResource(id = R.drawable.draft_close_circle_border),
+					contentDescription = stringResource(id = R.string.action_back),
+					tint = Color.Unspecified
+				)
 			}
 		}
 	}
--- app/src/main/java/me/tagavari/airmessage/compose/component/AttachmentQueueEntryMedia.kt
+++ app/src/main/java/me/tagavari/airmessage/compose/component/AttachmentQueueEntryMedia.kt
@@ -1,33 +1,33 @@
 package me.tagavari.airmessage.compose.component
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.memory.MemoryCache
+import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import me.tagavari.airmessage.messaging.QueuedFile
 
 @Composable
 fun AttachmentQueueEntryMedia(
 	queuedFile: QueuedFile,
-	onClick: () -> Unit
+	onClick: () -> Unit,
+	fallback: @Composable () -> Unit
 ) {
-	AsyncImage(
-		model = ImageRequest.Builder(LocalContext.current)
-			.data(queuedFile.file.either)
-			.crossfade(true)
-			.build(),
+	SubcomposeAsyncImage(
 		modifier = Modifier
 			.fillMaxHeight()
 			.clip(RoundedCornerShape(12.dp))
 			.clickable(onClick = onClick),
-		contentDescription = null
+		model = ImageRequest.Builder(LocalContext.current)
+			.data(queuedFile.file.either)
+			.crossfade(true)
+			.build(),
+		contentDescription = null,
+		error = { fallback() }
 	)
 }
