3fa89172c6e94a4ab81d981950ca300dd7d02207
Cole Feuer <13751213+tagavari@users.noreply.github.com>
2022-10-31 06:43:45 -0400

    Experiment with Kotlin multiplatform

---
+++ common/build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.compose.compose
+
+plugins {
+	kotlin("multiplatform")
+	id("org.jetbrains.compose")
+	id("com.android.library")
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+	android()
+	jvm("desktop") {
+		compilations.all {
+			kotlinOptions.jvmTarget = "11"
+		}
+	}
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				api(compose.runtime)
+				api(compose.foundation)
+				
+				@OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+				api(compose.material3)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(kotlin("test"))
+			}
+		}
+		val androidMain by getting {
+			dependencies {
+				api("androidx.appcompat:appcompat:1.2.0")
+				api("androidx.core:core-ktx:1.3.1")
+			}
+		}
+		val androidTest by getting {
+			dependencies {
+				implementation("junit:junit:4.13")
+			}
+		}
+		val desktopMain by getting {
+			dependencies {
+				api(compose.preview)
+			}
+		}
+		val desktopTest by getting
+	}
+}
+
+android {
+	compileSdk = 33
+	sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+	defaultConfig {
+		minSdk = 24
+		targetSdk = 33
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+}
